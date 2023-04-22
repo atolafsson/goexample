31a6498e0712ed8dd50afc820e848a24f7151ff6
atolafsson <atolafsson@gmail.com>
2023-04-21 22:14:06 -0500

    restructure

--- goexample.go
+++ goexample.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/atolafsson/goexample/database"
+	"github.com/atolafsson/goexample/handlers"
 	"github.com/gofiber/fiber/v2"
 )
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.ListFacts)
+	app.Post("/fact", handlers.CreateFact)
+}
 
 func main() {
 	database.ConnectDb()
